95425fab42cf953e7ac9976608068eb64da15851
saChoi <sola2440@gmail.com>
2020-11-30 10:46:40 +0900

    push 추가

--- SourceTreeStudy.cpp
+++ SourceTreeStudy.cpp
@@ -34,6 +34,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout<<"         <li>충돌</li>"<<endl;
 	cout<<"         <li>충돌 해결</li>"<<endl;
 	cout<<"         <li>원격저장소</li>"<<endl;
+    cout<<"         <li>push</li>"<<endl;
 	cout<<"		</ul>"<<endl;
 	cout<<"	</body>"<<endl;
 	cout<<"</html>"<<endl;
